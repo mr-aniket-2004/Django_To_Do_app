b66dd05a2728f2fa7a616245b2b9750b30191d63
Aniket Chandiwade <157163406+mr-aniket-2004@users.noreply.github.com>
2025-11-24 13:17:01 +0530

    commit

--- ToDoApp/mylist/urls.py
+++ ToDoApp/mylist/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("",views.home,name="home"),
+    path("add/", views.add_task, name="add_task"),
+    path("edit/", views.edit_task, name="edit_task"),
+    path("delete/<int:list_id>/", views.delete_task, name="delete_task"),
 ]
--- ToDoApp/mylist/views.py
+++ ToDoApp/mylist/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect , get_object_or_404
 from .models import Task
 
 # Create your views here.
@@ -6,3 +6,24 @@ from .models import Task
 def home(request):
     task =Task.objects.all().order_by("created_at")
     return render(request, "subpages/home.html",{"task":task})
+
+def add_task(request):
+
+    if request.method =="POST":
+        title = request.POST.get("title")
+        description = request.POST.get("Description")
+        if title :
+            Task.objects.create(title =title,description= description)
+            return redirect("home")
+        else:
+            error = "Title cannot be Empty"
+            return render(request, "subpages/add_task.html",{"error":error})
+            
+    return render(request, "subpages/add_task.html")
+
+def edit_task(request):
+    return render(request, "subpages/edit_task.html")
+
+def delete_task(request, list_id):
+    task = get_object_or_404(Task, id=list_id)
+    return render(request, "subpages/delete_task.html",{"task":task})
